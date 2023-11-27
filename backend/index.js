f98406cb850888d8ee6d38cb182f7e902dc0d004
Achille Maboa <achille.maboa@icloud.com>
2023-11-27 11:29:47 +0100

    effacer un livre

--- backend/index.js
+++ backend/index.js
@@ -103,6 +103,25 @@ app.get('/books', async (request, response) => {
   });
 
 
+  app.delete('/books/:id', async (request, response) => {
+    try {
+      const { id } = request.params;
+  
+      const result = await Book.findByIdAndDelete(id);
+  
+      if (!result) {
+        return response.status(404).json({ message: 'Book not found' });
+      }
+  
+      return response.status(200).send({ message: 'Book deleted successfully' });
+  
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
+
+
 
 mongoose
 .connect(mongoDBURL)
