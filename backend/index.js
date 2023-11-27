727157e731cd1fcfba47fa2e9a2c362a5e76acfd
Achille Maboa <achille.maboa@icloud.com>
2023-11-27 10:25:03 +0100

    sauvegarde nouveau livre mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {PORT, mongoDBURL} from "./config.js";
 import mongoose from "mongoose";
+import { Book } from './models/bookModel.js';
 
 const app = express();
 
@@ -9,6 +10,43 @@ app.get('/', (req,res)=> {
     return res.status(234).send('welcome To my world')
 
 });
+
+// Middleware  request body
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
+});
+
+// Route pour sauvebgarder un nouveau livre
+app.post('/books', async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: 'Send all required fields: title, author, publishYear',
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+
 
 mongoose
 .connect(mongoDBURL)
